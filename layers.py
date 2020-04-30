6450bff7819e4e6ffb4ee38aeadfe7eec62eb722
Piyush Vyas <pi.yush@icloud.com>
2020-04-29 23:05:03 -0400

    Add docs and remove init choice

--- layers.py
+++ layers.py
@@ -5,31 +5,47 @@ from torch.nn.parameter import Parameter
 
 
 class GraphConv(torch.nn.Module):
+    """ Performes Graph Convolution operation on incoming data X'^(l + 1) = (A'X^(l))W^(l) + b
     
-    """GCN layer, X^(l + 1) = (AX^(l))W^(l) + b """
+    Args:
+        in_features: size of each input sample
+        out_features: size of each output sample
+        bias: If set to ``False``, the layer will not learn an additive bias.
+            Default: ``True``
 
-    def __init__(self, in_features, out_features, bias=True, init='kaiming'):
+    Shape:
+        - Input: :math:`(N, *, H_{in})` where :math:`*` means any number of
+          additional dimensions and :math:`H_{in} = \text{in\_features}`
+        - Output: :math:`(N, *, H_{out})` where all but the last dimension
+          are the same shape as the input and :math:`H_{out} = \text{out\_features}`.
+
+    Attributes:
+        weight: the learnable weights of the module of shape
+                :math:`(\text{out\_features}, \text{in\_features})`. The values are
+                initialized from :math:`\mathcal{U}(-\text{bound}, \text{bound})` where
+                :math: `\text{bound} = \text{gain} \times \sqrt{\frac{3}{\text{fan\_mode}}}`
+        bias: the learnable bias of the module of shape
+              :math:`(\text{out\_features})`.
+              If :attr:`bias` is ``True``, the values are initialized with the scalar value `0`.
+    
+    """
+    
+    __constants__ = ['in_features, out_features']
+
+    def __init__(self, in_features, out_features, bias=True):
         super(GraphConv, self).__init__()
         self.in_features = in_features
         self.out_features = out_features
-        self.init = init
-        
-        # init weights & biases
         self.weight = Parameter(torch.Tensor(self.out_features, self.in_features))
-
         if bias:
             self.bias = Parameter(torch.Tensor(self.out_features))
+        else:
+            self.register_parameter('bias', None)
         self.reset_parameters()
     
 
     def reset_parameters(self):
-
-        if self.init == 'xavier':
-            init.xavier_uniform_(self.weight, gain=init.calculate_gain('relu'))
-
-        elif self.init == 'kaiming':
-            init.kaiming_uniform_(self.weight, nonlinearity='relu') 
-
+        init.kaiming_uniform_(self.weight, nonlinearity='relu') 
         if self.bias is not None:
             init.zeros_(self.bias)
 
@@ -45,5 +61,6 @@ class GraphConv(torch.nn.Module):
         return output
         
         
-        
+    def extra_repr(self):
+        return 'in_features={}, out_features={}, bias={}'.format(self.in_features, self.out_features, self.bias is not None)
         
