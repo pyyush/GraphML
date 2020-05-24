9f8c155c63ebd29914b19eb4b11e7452f4a45fa1
Piyush Vyas <pi.yush@icloud.com>
2020-05-24 16:09:16 -0400

    Update models.py

--- models.py
+++ models.py
@@ -3,50 +3,23 @@ from layers import GraphConv
 
 
 class GCN(torch.nn.Module):
-    def __init__(self, fan_in, nhid, nclass, dropout=0.2, normalize=True):
+    def __init__(self, fan_in, fan_out, layers, dropout=0.2, normalize=True, bias=False):
         super(GCN, self).__init__()
         self.fan_in = fan_in
-        self.nhid = nhid
-        self.nclass = nclass
+        self.fan_out = fan_out
+        self.num_layers = layers
+        self.dropout = dropout
         self.normalize = normalize
-        self.drop = torch.nn.Dropout(p=dropout)
-        self.norm = torch.nn.LayerNorm(normalized_shape=self.nhid)
-
-        # layers
-        self.gcn1 = GraphConv(self.fan_in, self.nhid)
-        self.gcn2 = GraphConv(self.nhid, self.nhid)
-        self.gcn3 = GraphConv(self.nhid, self.nhid)
-        self.gcn4 = GraphConv(self.nhid, self.nclass)
-    
+        self.bias = bias
+        self.network = torch.nn.ModuleList([GraphConv(in_features=fan_in[i], 
+                                                      out_features=fan_out[i], 
+                                                      dropout=self.dropout, 
+                                                      bias=self.bias, 
+                                                      normalize=self.normalize) for i in range(len(self.num_layers) - 1])
+        self.network.extend(GraphConv(in_features=fan_in[-1], out_features=fan_out[-1], bias=self.bias, last=True))
+        
+                                            
     def forward(self, sparse_adj, feats):
-        
-        feats = self.gcn1(sparse_adj, feats)
-        if self.normalize:
-            feats = self.norm(feats)
-        feats = torch.nn.functional.relu(feats)
-        feats = self.drop(feats)
-
-        #####################################
-        
-        feats = self.gcn2(sparse_adj, feats)
-        
-        if self.normalize:
-            feats = self.norm(feats)
-        feats = torch.nn.functional.relu(feats)
-        feats = self.drop(feats)
-
-        #####################################
-        
-        feats = self.gcn3(sparse_adj, feats)
-        
-        if self.normalize:
-            feats = self.norm(feats)
-        feats = torch.nn.functional.relu(feats)
-        feats = self.drop(feats)
-
-        #####################################
-        
-        out = self.gcn4(sparse_adj, feats)
-        
-        return torch.nn.functional.log_softmax(out, dim=1)
-    
+        for idx, layer in enumerate(self.network):
+            feats = layer(sparse_adj, feats)
+        return feats
