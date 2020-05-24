4242dd24643b922e92cf10138924408488d25688
Piyush Vyas <pi.yush@icloud.com>
2020-05-24 13:49:19 -0400

    make xavier uniform default init

--- layers.py
+++ layers.py
@@ -1,29 +1,26 @@
 import math
 import torch
-import torch.nn.init as init
 from torch.nn.parameter import Parameter
 
 
 class GraphConv(torch.nn.Module):
-    """ Performes Graph Convolution operation on incoming data X'^(l + 1) = (A'X^(l))W^(l) + b
+    """ Performes the Graph Convolution operation: math: `X' = \hat{A}XW`
     
     Args:
         in_features: size of each input sample
         out_features: size of each output sample
         bias: If set to ``False``, the layer will not learn an additive bias.
-            Default: ``True``
+            Default: ``False``
 
     Shape:
-        - Input: :math:`(N, *, H_{in})` where :math:`*` means any number of
-          additional dimensions and :math:`H_{in} = \text{in\_features}`
-        - Output: :math:`(N, *, H_{out})` where all but the last dimension
-          are the same shape as the input and :math:`H_{out} = \text{out\_features}`.
+        - Input: :math:`(N, H_{in})` where :math:`H_{in} = \text{in\_features}`
+        - Output: :math:`(N, H_{out})` where :math:`H_{out} = \text{out\_features}`.
 
     Attributes:
         weight: the learnable weights of the module of shape
                 :math:`(\text{out\_features}, \text{in\_features})`. The values are
                 initialized from :math:`\mathcal{U}(-\text{bound}, \text{bound})` where
-                :math: `\text{bound} = \text{gain} \times \sqrt{\frac{3}{\text{fan\_mode}}}`
+                :math: `\text{bound} = \sqrt{\frac{6}{\text{in\_features + out\_features}}}`
         bias: the learnable bias of the module of shape
               :math:`(\text{out\_features})`.
               If :attr:`bias` is ``True``, the values are initialized with the scalar value `0`.
@@ -32,7 +29,7 @@ class GraphConv(torch.nn.Module):
     
     __constants__ = ['in_features, out_features']
 
-    def __init__(self, in_features, out_features, bias=True):
+    def __init__(self, in_features, out_features, bias=False):
         super(GraphConv, self).__init__()
         self.in_features = in_features
         self.out_features = out_features
@@ -42,13 +39,13 @@ class GraphConv(torch.nn.Module):
         else:
             self.register_parameter('bias', None)
         self.reset_parameters()
-    
-
+   
     def reset_parameters(self):
-        init.kaiming_uniform_(self.weight, nonlinearity='relu') 
+        # Xavier Glorot Uniform
+        bound = math.sqrt(6.0/float(self.out_features + self.in_features))
+        self.weight.data.uniform_(-bound, bound)
         if self.bias is not None:
-            init.zeros_(self.bias)
-
+            self.bias.data.fill_(0)
 
     def forward(self, A, X):
 
@@ -59,7 +56,6 @@ class GraphConv(torch.nn.Module):
             output += self.bias
             
         return output
-        
         
     def extra_repr(self):
         return 'in_features={}, out_features={}, bias={}'.format(self.in_features, self.out_features, self.bias is not None)
