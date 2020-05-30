0e1ced423516362d382fcd138faea04925f27ba6
Piyush Vyas <pi.yush@icloud.com>
2020-05-30 02:20:04 -0400

    remove ppi support

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ parser.add_argument('--exp_num', type=str, help='experiment number for tensorboa
 parser.add_argument('--test', type=int, default=-1, help='True if 1, else False')
 parser.add_argument('--batch_size', type=int, default=10)
 parser.add_argument('--num_clusters_train', type=int, default=15000)
-parser.add_argument('--num_clusters_val', type=int, default=2)
 parser.add_argument('--num_clusters_test', type=int, default=1)
 parser.add_argument('--layers', type=int, default=4, help='Number of layers in the network.')
 parser.add_argument('--epochs', type=int, default=400, help='Number of epochs to train.')
@@ -37,12 +36,8 @@ writer = SummaryWriter('runs/' + args.dataset + '/' + args.exp_num)
 if args.dataset == 'amazon2M' and args.layers == 4: 
     _in = [100, args.hidden, args.hidden, args.hidden]
     _out = [args.hidden, args.hidden, args.hidden, 47]
+
     
-if args.dataset == 'ppi' and args.layers == 5:
-    _in = [50, args.hidden, args.hidden, args.hidden, args.hidden]
-    _out = [args.hidden, args.hidden, args.hidden, args.hidden, 121]
-
-
 def train(model, criterion, optimizer, features, adj, labels, dataset):
         optimizer.zero_grad()
         
@@ -55,11 +50,7 @@ def train(model, criterion, optimizer, features, adj, labels, dataset):
         adj = torch.sparse.FloatTensor(i.t(), v, adj[2]).cuda()
         
         output = model(adj, features)
-        
-        if dataset == 'ppi':
-            loss = criterion(output, labels.type_as(output))
-        else:    
-            loss = criterion(output, torch.max(labels, 1)[1])
+        loss = criterion(output, torch.max(labels, 1)[1])
 
         loss.backward()
         optimizer.step()
@@ -68,7 +59,7 @@ def train(model, criterion, optimizer, features, adj, labels, dataset):
 
 
 @torch.no_grad()
-def test(model, features, adj, labels, mask, device, dataset):
+def test(model, features, adj, labels, mask, device):
         model.eval()
         
         features = torch.FloatTensor(features).to(device)
@@ -87,14 +78,10 @@ def test(model, features, adj, labels, mask, device, dataset):
         
         output = model(adj, features)
         
-        if dataset == 'ppi':
-            f1 = utils.f1_score(labels[mask].cpu().numpy(), output[mask].cpu().numpy(), multitask=True)
-        else:
-            pred = output[mask].argmax(dim=1, keepdim=True)
-            labels = torch.max(labels[mask], 1)[1]
-            f1 = utils.f1_score(labels.cpu().numpy(), pred.cpu().numpy())
+        pred = output[mask].argmax(dim=1, keepdim=True)
+        labels = torch.max(labels[mask], 1)[1]
+        return sklearn.metrics.f1_score(labels.cpu().numpy(), pred.cpu().numpy(), average='micro')
 
-        return f1
 
 
 def main():
@@ -106,24 +93,14 @@ def main():
     
     # Prepare Train Data
     start = time.time()
-    if args.batch_size > 1: # amazon2M will go here
-        _, parts = utils.partition_graph(train_adj, train_nodes, args.num_clusters_train)
-        parts = [np.array(pt) for pt in parts]
-        train_features, train_support, y_train = utils.preprocess_multicluster(train_adj, parts, train_feats, y_train, args.num_clusters_train, args.batch_size)    
-    else: # ppi will go here
-        parts, train_features, train_support, y_train, _ = utils.preprocess(train_adj, train_feats, y_train, train_nodes, args.num_clusters_train)
+    _, parts = utils.partition_graph(train_adj, train_nodes, args.num_clusters_train)
+    parts = [np.array(pt) for pt in parts]
+    train_features, train_support, y_train = utils.preprocess_multicluster(train_adj, parts, train_feats, y_train, args.num_clusters_train, args.batch_size)    
     print('Train Data pre-processed in {:.2f} seconds!'.format(time.time() - start))
-    
-    # Prepare Val Data
-    if args.dataset == 'ppi':
-        start = time.time()
-        _, val_features, val_support, y_val, val_mask = utils.preprocess(_adj, _feats, y_val, np.arange(N), args.num_clusters_val, val_mask)
-        print('Val Data pre-processed in {:.2f} seconds!'.format(time.time() - start))
     
     # Prepare Test Data
     if args.test == 1:    
-        if args.dataset == 'amazon2M': # amazon2M val is test
-            y_test, test_mask = y_val, val_mask
+        y_test, test_mask = y_val, val_mask
         start = time.time()
         _, test_features, test_support, y_test, test_mask = utils.preprocess(_adj, _feats, y_test, np.arange(N), args.num_clusters_test, test_mask) 
         print('Test Data pre-processed in {:.2f} seconds!'.format(time.time() - start))
@@ -135,10 +112,7 @@ def main():
     model.cuda()
 
     # Loss Function
-    if args.dataset == 'ppi': # ppi is MultiLabel
-        criterion = torch.nn.BCEWithLogitsLoss()
-    else:
-        criterion = torch.nn.CrossEntropyLoss()
+    criterion = torch.nn.CrossEntropyLoss()
     
     # Optimization Algorithm
     optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
@@ -163,21 +137,9 @@ def main():
         writer.add_scalar('time/train', time.time() - start, epoch)
         writer.add_scalar('loss/train', avg_loss/len(train_features), epoch)
         
-        # Validate on gpu
-        if args.dataset == 'ppi':
-            avg_f1 = 0
-            start = time.time()
-            for batch in range(len(val_features)):
-                f1 = test(model, val_features[batch], val_support[batch], y_val[batch], val_mask[batch], device='cuda', dataset=args.dataset)
-                avg_f1 += f1
-                
-            # Write Validation stats to tensorboard
-            writer.add_scalar('time/val', time.time() - start, epoch)
-            writer.add_scalar('f1/val', avg_f1/len(val_features), epoch)
-            
     if args.test == 1:    
         # Test on cpu
-        f1 = test(model.cpu(), test_features, test_support, y_test, test_mask, device='cpu', dataset=args.dataset)
+        f1 = test(model.cpu(), test_features, test_support, y_test, test_mask, device='cpu')
         print('f1: {:.4f}'.format(f1))
      
         
