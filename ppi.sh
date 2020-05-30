6a16b300f2d9c3d987292631292d0cd6ab10fc6d
Piyush Vyas <pi.yush@icloud.com>
2020-05-30 02:09:20 -0400

    Delete ppi.sh

--- ppi.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-export METIS_DLL=~/.local/lib/libmetis.so
-
-python main.py --dataset ppi --exp_num 1 --batch_size 1 --num_clusters_train 50 --num_clusters_val 2 --num_clusters_test 1 --layers 5 --epochs 400 --lr 0.01 --hidden 512 --dropout 0.2 --lr_scheduler -1 --test 1
-
