7e9815ad3f48e3b20f6e9ac8bf19c485a9e97267
Piyush Vyas <pi.yush@icloud.com>
2020-06-15 00:59:36 -0400

    Update amazon2M.sh

--- amazon2M.sh
+++ amazon2M.sh
@@ -2,5 +2,5 @@
 
 export METIS_DLL=~/.local/lib/libmetis.so
 
-python main.py --dataset amazon2M --exp_num 1 --batch_size 20 --num_clusters_train 20000 --num_clusters_test 1 --layers 4 --epochs 200 --lr 0.01 --hidden 400 --dropout 0.2 --lr_scheduler 1 --test 1
+python main.py --dataset amazon2M --exp_num 1 --batch_size 20 --num_clusters_train 10000 --num_clusters_test 1 --layers 4 --epochs 200 --lr 0.01 --hidden 2048 --dropout 0.2 --lr_scheduler -1 --test 1
 
